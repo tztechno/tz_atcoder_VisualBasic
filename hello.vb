956218acd1dd96d2fc283fe99107b9616d1a4ef7
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-26 14:52:52 +0900

    Create hello.vb

---
+++ hello.vb
@@ -0,0 +1,12 @@
+Module HelloWorld
+    Sub Main()
+        Dim S As String
+        S = Console.ReadLine()
+
+        If S = "Hello,World!" Then
+            Console.WriteLine("AC")
+        Else
+            Console.WriteLine("WA")
+        End If
+    End Sub
+End Module
